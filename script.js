eb8b9470ab024fac5d385aba46adca7c8ae898ec
Carson Haskell <carson.haskell@gmail.com>
2023-01-12 23:10:38 -0800

    add sketch functionality and grid line toggle
    Sketch feature is fully functional with variable size inputted by the
    user via canvas size button. Can also toggle grid lines on or off.

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-const container = document.getElementById('sketch-canvas');
+const sketchCanvas = document.getElementById('sketch-canvas');
+const sketchButton = document.getElementById('sketch-scale');
+const toggleButton = document.getElementById('toggle-grid');
 
 // Generates the etch-a-sketch canvas
-makeRows(48);
+createCanvas(16);
 
 // Fills each square as mouse hovers creating a drawing effect
-const grid = document.querySelectorAll('.grid-item');
-grid.forEach(square => square.addEventListener('mouseover', hover));
+enableSketch();
+
+let gridScale;
+sketchButton.addEventListener('click', () => {
+  // Grid cannot exceed 100 without slowing computer
+  do {
+    gridScale = parseInt(prompt());
+  } while (gridScale < 2 || gridScale > 100);
+
+  // Delete previous canvas and generate new one
+  sketchCanvas.textContent = '';
+  createCanvas(gridScale);
+  enableSketch();
+});
+
+toggleButton.addEventListener('click', toggleLines);
 
 
-function makeRows(gridSize) {
-    rows = gridSize;
-    cols = gridSize;
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < rows * cols; c++) {
-      let cell = document.createElement('div');
-      container.appendChild(cell).className = 'grid-item';
+// Abstracted functions below
+
+function createCanvas(gridSize) {
+  rows = gridSize;
+  cols = gridSize;
+  sketchCanvas.style.setProperty('--grid-rows', rows);
+  sketchCanvas.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement('div');
+    sketchCanvas.appendChild(cell).className = 'grid-item';
+  }
+}
+
+function toggleLines() {
+  const grid = document.querySelectorAll('.grid-item');
+  grid.forEach((square) => {
+    if (square.style.border === 'none') {
+      square.style.border = '1px solid #ddd';
+    } else {
+      square.style.border = 'none';
     }
-  }
+  });
+}
 
-  function hover() {
-    this.style.backgroundColor = 'green';
-  }
+function enableSketch() {
+  const grid = document.querySelectorAll('.grid-item');
+  grid.forEach((square) =>
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'green';
+    })
+  );
+}
